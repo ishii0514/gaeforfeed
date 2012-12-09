b00953e82f63d25ec9dd72249962b42f3c7f74e0
Yosuke Ishii <ishii0514@gmail.com>
2012-12-10 02:14:51 +0900

    redirect to localhost

--- feeds.py
+++ feeds.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
+#from google.appengine.ext import webapp
+#from google.appengine.ext.webapp.util import run_wsgi_app
+import webapp2
 from google.appengine.ext.webapp import template
 import urllib
+import urllib2
 import os
 import cgi
+import json
 
 #cfeed info
 consumer_key = '3MVG98XJQQAccJQcNdyHKdTAAktiZJU3MEJAH3gCfQqOl9xoAc_TUky1UMRDzxLvxgRp9GOCxQFmxYKDtvwZF'
@@ -18,37 +21,43 @@ initialpage = 'http://localhost:8081/tile'
 
 
 
-class JsonAjax(webapp.RequestHandler):
+class JsonAjax(webapp2.RequestHandler):
     def get(self):
         template_values = {}
         path = os.path.join(os.path.dirname(__file__), 'json_ajax.html')
         self.response.out.write(template.render(path, template_values))
         
-class OutputJSON(webapp.RequestHandler):
+class OutputJSON(webapp2.RequestHandler):
     def get(self):
         title = self.request.get('title').encode('UTF-8')
         link = self.request.get('link').encode('UTF-8')
         res = '{"title":"%s", "link":"%s"}' % (title, link)
         self.response.out.write(cgi.escape(unicode(res, 'UTF-8')))
         
-class Demo(webapp.RequestHandler):
+class Demo(webapp2.RequestHandler):
     def get(self):
         template_values = {'name' : 'yosuke'}
         path = os.path.join(os.path.dirname(__file__), 'demo.html')
         self.response.out.write(template.render(path, template_values))
 
-class Tile(webapp.RequestHandler):
+class Tile(webapp2.RequestHandler):
     def get(self):
-        template_values = {'name' : 'tile demo'}
-        path = os.path.join(os.path.dirname(__file__), 'tile.html')
-        self.response.out.write(template.render(path, template_values))
+        id = self.request.get('id').encode('UTF-8')
+        instance_url = self.request.get('instance_url').encode('UTF-8')
+        
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('tile id:%s,instance_url:%s' % (id,instance_url))
+        
+        #template_values = {'name' : 'tile demo'}
+        #path = os.path.join(os.path.dirname(__file__), 'tile.html')
+        #self.response.out.write(template.render(path, template_values))
                     
-class NextPage(webapp.RequestHandler):
+class NextPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write('NextPage!')
 
-class SignIn(webapp.RequestHandler):
+class SignIn(webapp2.RequestHandler):
     def get(self):
         auth_url = 'https://login.salesforce.com/services/oauth2/authorize'
         params = {
@@ -60,11 +69,42 @@ class SignIn(webapp.RequestHandler):
         auth_url = auth_url + '?' + params
         self.redirect(auth_url)
             
-class CallBack(webapp.RequestHandler):
+class CallBack(webapp2.RequestHandler):
     def get(self):
-        self.redirect(initialpage)
+        code = self.request.get('code').encode('UTF-8')
+        auth_url = 'https://login.salesforce.com/services/oauth2/token'
+        params = {
+            'code' : code,
+            'grant_type' : 'authorization_code',
+            'client_id' : consumer_key,
+            'client_secret' : consumer_secret,
+            'redirect_uri': redirect_uri,
+        }
+        params = urllib.urlencode(params).encode()
+        request = urllib2.Request(auth_url)
+        request.add_data(params)
+        response = urllib2.urlopen(request)
+        res = response.read()
         
-application = webapp.WSGIApplication([('/', SignIn),
+        data = json.loads(res.decode())
+        
+        params = {
+            'id' : data['id'],
+            'instance_url' : data['instance_url'],
+            'refresh_token': data['refresh_token'],
+            'signature' : data['signature'],
+            'access_token' :data['access_token'],
+        }
+        
+        params = urllib.urlencode(params)
+        redirect = initialpage + '?' + params                
+        self.redirect(redirect) 
+
+        
+
+        
+        
+app = webapp2.WSGIApplication([('/', SignIn),
                                       ('/ajaxtest',JsonAjax),
                                       ('/OutputJSON',OutputJSON),
                                       ('/demo', Demo),
@@ -73,9 +113,10 @@ application = webapp.WSGIApplication([('/', SignIn),
                                       ('/signin', SignIn),
                                       ('/callback', CallBack)], debug=True)
 
-
+'''
 def main():
     run_wsgi_app(application)
 
 if __name__ == "__main__":
     main()
+'''
---
+++ js/tile.js
@@ -0,0 +1,97 @@
+var $containers = $(".container");
+
+// This just renders us some random temporary children
+function renderChildren(placekitten) {
+  $containers.children().filter(":not(.credits)").remove();
+  $containers.each(function(container_i) {
+    for(i=0;i<15;i++) {
+      var $element = $("<div class='object'></div>"),
+          height = Math.floor(Math.random() * 200) + 100,
+          width = $containers.children().first().width();
+      if(container_i === 1) { height = 140; }
+      if(placekitten) {
+        var background = 'url("http://www.placekitten.com/'+width+'/'+height+'")';
+      } else {
+        var background = getRandomColor(),
+            $img = $('<img src="http://fpoimg.com/'+width+'x'+Math.floor(height / 2)+'" class="placeholder" />');
+        $element.append($img);
+      }
+      $element.css({ background: background, height: height });
+      $(this).append($element);
+    }
+  });
+}
+renderChildren(false);
+
+function getRandomColor() {
+  var letters = 'ABCDEF'.split('');
+  var color = '#';
+  for (var i=0;i<6;i++) {
+    color += letters[Math.round(Math.random() * 5)];
+  }
+  return color;
+}
+
+// Initial Shapeshift
+$containers.shapeshift({
+  paddingY: 20
+});
+
+// When our buttons are clicked.
+$(".filter").on("click", function(e) {
+  e.preventDefault();
+
+  if($(this).hasClass("no-drag-animate")) {
+    $containers.shapeshift({
+      enableDragAnimation: false,
+      paddingY: 20
+    });
+  }
+  if($(this).hasClass("no-drag")) {
+    $containers.shapeshift({
+      enableDrag: false,
+      paddingY: 20
+    });
+  }
+  if($(this).hasClass("drag")) {
+    $containers.shapeshift({
+      paddingY: 20
+    });
+  }
+  if($(this).hasClass("hide")) {
+    var $objects = $containers.children().filter(":visible"),
+        random = Math.round(Math.random() * $objects.size());
+    $objects.eq(random).hide();
+    $containers.shapeshift({
+      paddingY: 20
+    });
+  }
+  if($(this).hasClass("show")) {
+    var $objects = $containers.children().filter(":hidden"),
+        random = Math.round(Math.random() * $objects.size());
+    $objects.eq(random).show();
+    $containers.shapeshift({
+      paddingY: 20
+    });
+  }
+  if($(this).hasClass("placekittens")) {
+    renderChildren(true);
+    $containers.shapeshift({
+      paddingY: 20
+    });
+  }
+})
+
+$containers.on("ss-event-dropped", function(e, selected) {
+  // Get the new position for the dropped item
+  var position = $(selected).index();
+  console.log("The dropped position:", position)
+
+  // Get the index position of each object
+  console.log("Child positions:")
+  $objects = $(this).children();
+  $objects.each(function(i) {
+    position[i] = $(this).index();
+    console.log($(this).index())
+  });
+});
